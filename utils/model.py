d63dc7d5f59a6efeee7a46a49eb883142400fe67
Miki Pregaard <48087675+Miki0110@users.noreply.github.com>
2023-07-15 13:58:50 +0200

    cleaned the model file a tiny bit

--- utils/model.py
+++ utils/model.py
@@ -19,7 +19,7 @@ sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 class GeoLocationClassifier(torch.nn.Module):
     """
     GeoLocationClassifier class
-    Uses the resnet50 model from torchvision
+    Uses the resnet101_2 model from torchvision
     """
     def __init__(self, num_classes):
         super(GeoLocationClassifier, self).__init__()
@@ -57,8 +57,19 @@ def calculate_accuracy(outputs, labels):
 
 
 def train_model(model, dataloader, criterion, optimizer, num_epochs):
+    """
+    Trainer for the model
+    Args:
+        model (nn.Module): Model used to train on
+        dataloader (torch.data.dataloader): Dataloader for the training set
+        criterion (nn.Module.loss): Loss function
+        optimizer (torch.optim): optimizer standard is to just use Adam
+        num_epochs (int): Number of epochs
+    """
+    # Set up a Summarywriter
     curr_time = datetime.now().strftime("%H%M%S")
     writer = SummaryWriter(f'runs/training_{curr_time}')
+
     model.train()  # set the model to training mode
     for epoch in range(num_epochs):
         running_loss = 0.0
@@ -66,6 +77,7 @@ def train_model(model, dataloader, criterion, optimizer, num_epochs):
         total_samples = 0
         progress_bar = tqdm(enumerate(dataloader), total=len(dataloader))
         for i, (inputs, labels) in progress_bar:
+            # Set the tensor to the right device
             inputs = inputs.to(model.device)
             labels = labels.to(model.device)
 
@@ -80,22 +92,25 @@ def train_model(model, dataloader, criterion, optimizer, num_epochs):
             loss.backward()
             optimizer.step()
 
+            # Calculate the running loss
             running_loss += loss.item() * inputs.size(0)
-
-            _, preds = torch.max(outputs, 1)
-            running_corrects += torch.sum(preds == labels.data)
+            # Calculate the running accuracy
+            _, preds = torch.max(outputs, 1)  # Get the best guess from the output
+            running_corrects += torch.sum(preds == labels.data)  # Check how many are correct
             total_samples += labels.size(0)
 
             progress_bar.set_description(
-                f'Epoch {epoch + 1}/{num_epochs} Loss: {loss.item():.4f} Accuracy: {running_corrects.double() / total_samples:.4f}')
+                f'Epoch {epoch + 1}/{num_epochs} Loss: {loss.item():.4f} '
+                f'Accuracy: {running_corrects.double() / total_samples:.4f}')
 
             # Delete tensors that are no longer needed
             del inputs, labels, outputs, loss
-
+        # Print the average scores
         epoch_loss = running_loss / len(dataloader.dataset)
         epoch_acc = running_corrects.double() / total_samples
-        print(
-            f'Epoch {epoch + 1}/{num_epochs} Average Loss: {epoch_loss:.4f} Average Accuracy: {epoch_acc:.4f}')
+        print(f'Epoch {epoch + 1}/{num_epochs} Average Loss: {epoch_loss:.4f} '
+            f'Average Accuracy: {epoch_acc:.4f}')
+
         # Write it into the tensorboard
         writer.add_scalar('Training Loss', epoch_loss, epoch)
         writer.add_scalar('Training Accuracy', epoch_acc, epoch)
@@ -109,32 +124,13 @@ def train_model(model, dataloader, criterion, optimizer, num_epochs):
             'loss': epoch_loss,
             'accuracy': epoch_acc,
         }, f'model_checkpoints/checkpoint_{epoch}.pth')
-
+        # Clear memory
         torch.cuda.empty_cache()
         gc.collect()
 
     print('Training complete')
     writer.close()
     return model
-
-
-def test_model(model, dataloader, criterion):
-    model.eval()  # set the model to evaluation mode
-    running_loss = 0.0
-    with torch.no_grad():
-        progress_bar = tqdm(enumerate(dataloader), total=len(dataloader))
-        for i, (inputs, labels) in progress_bar:
-            inputs = inputs.to(model.device)
-            labels = labels.to(model.device)
-
-            # forward
-            outputs = model(inputs)
-            loss = criterion(outputs, labels)
-
-            running_loss += loss.item() * inputs.size(0)
-            progress_bar.set_description(f'Test Loss: {loss.item():.4f}')
-        average_loss = running_loss / len(dataloader.dataset)
-        print(f'Average Test Loss: {average_loss:.4f}')
 
 
 # Debugging to check if the model works and cuda is available
